b7408b963d22489697c921f107e5ee75f2773248
LearnCode <rajsingh987384@gmail.com>
2025-05-03 22:32:29 +0530

    customize pages

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,8 +1,6 @@
-import { requireAuthSession } from "@/lib/auth/requireAuthSession";
 import CheckoutForm from "./checkout-form";
 
 const Checkout = async () => {
-  await requireAuthSession();
   return (
     <div className="text-black min-h-[100vh] bg-gray-100 px-5 py-8">
       <CheckoutForm />
